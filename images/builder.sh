73fc6811258960cc86b72b41ba9aefe3dd84a865
dougbtv <dosmith@redhat.com>
2021-09-16 15:46:41 -0400

    Remove obsolete images directory

--- images/builder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $(pwd) == *images ]]; then 
-  echo "you should run this from the clone's root";
-  exit 1;
-fi
-
-rsync -av --progress . ./images/supported/ --exclude images
-rsync -av --progress . ./images/unsupported/ --exclude images
